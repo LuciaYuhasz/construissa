b5343494a42df8387a912b631087d377128a7770
Marilena Escudero <marusofok2@gmail.com>
2023-10-12 11:21:44 -0300

    lista de herramientas y baja de empleado

--- src/construissa/Construissa.java
+++ src/construissa/Construissa.java
@@ -45,34 +45,66 @@ public class Construissa {
 //           if(filas>0){
 //           JOptionPane.showMessageDialog(null,"Empleado agregado");}
            
-        //3)  
-         String sql="insert into empleado (dni,apellido,nombre,acceso,estado)"
-                    +"values(26584125,'Fernandez','Camila','1',true)";
-           PreparedStatement ps= con.prepareStatement(sql);
-           
-           int filas= ps.executeUpdate();
-           if(filas>0){
-           JOptionPane.showMessageDialog(null,"Empleado agregado");}
-           
-           
-//       2)
-//            String sql="insert into alumno (dni,apellido,nombre,fechaDeNacimiento,estado)"
-//                    +"values(22456879,'Guitierrez','Roberto','1860-08-24',true)";
+//        //3)  
+//         String sql="insert into empleado (dni,apellido,nombre,acceso,estado)"
+//                    +"values(26584125,'Fernandez','Camila','1',true)";
 //           PreparedStatement ps= con.prepareStatement(sql);
-//            int filas= ps.executeUpdate();
-//          if(filas>0){
-//           JOptionPane.showMessageDialog(null,"Alumno agregado");}
-//       3)
-//
-//                 String sql="insert into alumno (dni,apellido,nombre,fechaDeNacimiento,estado)"
-//                   +"values(56988421,'Fonseca','Andrea','1863-12-05',true)";
-//          PreparedStatement ps= con.prepareStatement(sql);
 //           
 //           int filas= ps.executeUpdate();
 //           if(filas>0){
-//           JOptionPane.showMessageDialog(null,"Alumno agregado");}
-         
-      }catch (ClassNotFoundException ex) {
+//           JOptionPane.showMessageDialog(null,"Empleado agregado");}
+
+//     INGRESO DE LAS 2 HERRAMIENTAS      
+           
+//             String sql = "insert into herramienta (nombre,descripcion,stock,estado)"
+//                     + "values('Soldadora','soldadora punta diamante Steel','10',true)";
+//             PreparedStatement ps = con.prepareStatement(sql);
+//
+//             int filas = ps.executeUpdate();
+//             if (filas > 0) {
+//                 JOptionPane.showMessageDialog(null, "Herramienta agregada");
+//             }
+//             
+//              String sql1 = "insert into herramienta (nombre,descripcion,stock,estado)"
+//                     + "values('motosierra','motosierra 500 wts','5',true)";
+//             PreparedStatement ps1 = con.prepareStatement(sql1);
+//
+//             int filas1 = ps1.executeUpdate();
+//             if (filas > 0) {
+//                 JOptionPane.showMessageDialog(null, "Herramienta agregada");
+//             }
+//
+//             String sql = "SELECT `idHerramienta`, `nombre`, `descripcion`, `stock`, `estado` FROM `herramienta`"
+//                     + "WHERE stock > 10";
+//             PreparedStatement ps = con.prepareStatement(sql);
+//
+//           
+//            ResultSet rs = ps.executeQuery();
+//            while (rs.next()) {
+//               
+//                int id = rs.getInt("idherramienta");
+//                String nombre = rs.getString("nombre");
+//                String descripcion = rs.getString("descripcion");
+//                int stock = rs.getInt("stock");
+//                boolean estado = rs.getBoolean("estado");
+//                System.out.println("-----------------------");
+//                System.out.println("id :" + id);
+//                System.out.println("Nombre:" + nombre);
+//                System.out.println("Descripcion :" + descripcion);
+//                System.out.println("Stock :" + stock );
+//                System.out.println("Estado : " + estado);
+//                System.out.println("-----------------------");
+//            } 
+//         Dar de baja al primer empleado cargado en la base 
+
+             String sql = " UPDATE empleado set estado = 0  WHERE idEmpleado = 1";
+             PreparedStatement ps = con.prepareStatement(sql);
+             int filas = ps.executeUpdate();
+             if (filas > 0) {
+                 JOptionPane.showMessageDialog(null, "Empleado dado de baja");
+             }
+
+         } catch (ClassNotFoundException ex) {
             JOptionPane.showMessageDialog(null, "Error de carga de base de datos " + ex.getMessage());
             }catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error en la conexion" + ex.getMessage());
